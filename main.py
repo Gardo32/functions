a61f00db7cfd3ba29b14c6f62b17f98f59450610
Gardo32 <gardodaqqaq@gmail.com>
2024-07-07 13:45:54 +0000

    add branch

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from forms import LoginForm
+from forms import LoginForm  # Assuming you have a LoginForm defined in forms.py
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-password = os.getenv('PASSWORD')
+
+# Dictionary mapping passwords to usernames
+password_to_user = {
+    'password_a': 'User A',
+    'password_b': 'User B',
+    # Add more as needed
+}
 
 app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -14,15 +20,14 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-# Mock database
-users = {'admin': {'password': password}}
-
+# Mock user database
 class User(UserMixin):
     def __init__(self, username):
         self.id = username
 
 @login_manager.user_loader
 def load_user(user_id):
+    # Load the User object based on the provided username
     return User(user_id)
 
 @app.route('/')
@@ -34,9 +39,9 @@ def home():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        password = form.password.data
-        username = 'admin'  # Since we are removing username input, using a default username
-        if username in users and users[username]['password'] == password:
+        password_attempt = form.password.data
+        if password_attempt in password_to_user:
+            username = password_to_user[password_attempt]
             user = User(username)
             login_user(user)
             return redirect(url_for('home'))
@@ -53,9 +58,7 @@ def logout():
 @app.route('/gpa')
 @login_required
 def gpa():
-
     return "GPA"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
