8d9aa2ef39c4841591719c3880db97f920f630f4
gardodaqqaq <gardodaqqaq@gmail.com>
2024-07-07 11:34:43 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from forms import LoginForm
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+password = os.getenv('PASSWORD')
+
+app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# Mock database
+users = {'admin': {'password': password}}
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/')
+@login_required
+def home():
+    return render_template('home.html', name=current_user.id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        password = form.password.data
+        username = 'admin'  # Since we are removing username input, using a default username
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid credentials', 401
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/gpa')
+@login_required
+def gpa():
+
+    return "GPA"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
